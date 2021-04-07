1fb5c100e40b95792dd02e1d4b2b1a9816c66c19
Michael Cordeiro <62896592+michaelcordeiro@users.noreply.github.com>
2021-04-07 00:57:49 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.get('/', (request, response) => {
+    return response.send('ok')
+})
+
 app.listen(8080)
