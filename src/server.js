1e5cd0ffff7c5c03d2c1ed5884a2c2ef81e7a563
Michael Cordeiro <62896592+michaelcordeiro@users.noreply.github.com>
2021-04-09 23:47:02 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,28 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 
+
+const passangers = [
+    {
+        name: "Milena",
+        flightNumber: 7859,
+        time: "18h00",
+    },
+    {
+        name: "Marcos",
+        flightNumber: 7859,
+        time: "18h00",
+    },
+    {
+        name: "Thaís",
+        flightNumber: 7859,
+        time: "18h00",
+    },
+];
+
+
 app.get('/', (request, response) => {
-    return response.send('ok')
+    return response.send(passangers)
 })
 
 app.listen(8080)
